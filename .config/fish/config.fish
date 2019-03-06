2273f2c5e50f9e3b07693789d8c94cc76578eada
yusuke furuta <yusukefulltype@gmail.com>
2019-03-07 02:57:25 +0900

    add alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 # anaconda
-set PATH /home/yusukefulltype/anaconda3/bin $PATH
-source /home/yusukefulltype/anaconda3/etc/fish/conf.d/conda.fish
+set PATH ~/anaconda3/bin $PATH
+source ~/anaconda3/etc/fish/conf.d/conda.fish
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
 set HISTSIZE 50000
@@ -19,9 +19,28 @@ alias egrep 'egrep --color=auto'
 alias ll 'ls -alF'
 alias la 'ls -A'
 alias l 'ls -CF'
+# 移動/コピー/削除時に、経過を表示
+alias cp 'cp -v'
+alias mv 'mv -v'
+alias rm 'rm -v'
+# 単位をGB,MB,KBにする
+alias df 'df -h'
 
 # alias for jupyter notebook
 alias jnote 'jupyter notebook'
 # alias file or folder open
 alias open 'xdg-open'
 
+# bat config
+set BAT_CONFIG_PATH ~/.bat/bat.conf
+
+# add go-lang path
+set PATH /usr/local/go/bin $PATH
+
+# add snap app path
+set PATH /snap/bin $PATH
+
+# set PAGER
+export PAGER=less
+
+export LC_COLLATE='C'
