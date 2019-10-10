5eb3da085481886656d38f824c70b321c2bdf671
yusuke furuta <yusukefulltype@gmail.com>
2019-10-10 22:30:56 +0900

    [format] フォーマッタの適応

--- dotfiles_Windows_use/.hyper.js
+++ dotfiles_Windows_use/.hyper.js
@@ -6,83 +6,84 @@ module.exports = {
   config: {
     // choose either `'stable'` for receiving highly polished,
     // or `'canary'` for less polished but more frequent updates
-    updateChannel: 'stable',
+    updateChannel: "stable",
 
     // default font size in pixels for all tabs
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily:
+      'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
-    fontWeight: 'normal',
+    fontWeight: "normal",
 
     // font weight for bold characters: 'normal' or 'bold'
-    fontWeightBold: 'bold',
+    fontWeightBold: "bold",
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorColor: "rgba(248,28,229,0.8)",
 
     // terminal text color under BLOCK cursor
-    cursorAccentColor: '#000',
+    cursorAccentColor: "#000",
 
     // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
-    cursorShape: 'BLOCK',
+    cursorShape: "BLOCK",
 
     // set to `true` (without backticks and without quotes) for blinking cursor
     cursorBlink: false,
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: "#fff",
 
     // terminal background color
     // opacity is only supported on macOS
-    backgroundColor: '#000',
+    backgroundColor: "#000",
 
     // terminal selection color
-    selectionColor: 'rgba(248,28,229,0.3)',
+    selectionColor: "rgba(248,28,229,0.3)",
 
     // border color (window, tabs)
-    borderColor: '#333',
+    borderColor: "#333",
 
     // custom CSS to embed in the main window
-    css: '',
+    css: "",
 
     // custom CSS to embed in the terminal window
-    termCSS: '',
+    termCSS: "",
 
     // if you're using a Linux setup which show native menus, set to false
     // default: `true` on Linux, `true` on Windows, ignored on macOS
-    showHamburgerMenu: '',
+    showHamburgerMenu: "",
 
     // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
     // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
-    showWindowControls: '',
+    showWindowControls: "",
 
     // custom padding (CSS format, i.e.: `top right bottom left`)
-    padding: '12px 14px',
+    padding: "12px 14px",
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#C51E14',
-      green: '#1DC121',
-      yellow: '#C7C329',
-      blue: '#0A2FC4',
-      magenta: '#C839C5',
-      cyan: '#20C5C6',
-      white: '#C7C7C7',
-      lightBlack: '#686868',
-      lightRed: '#FD6F6B',
-      lightGreen: '#67F86F',
-      lightYellow: '#FFFA72',
-      lightBlue: '#6A76FB',
-      lightMagenta: '#FD7CFC',
-      lightCyan: '#68FDFE',
-      lightWhite: '#FFFFFF',
+      black: "#000000",
+      red: "#C51E14",
+      green: "#1DC121",
+      yellow: "#C7C329",
+      blue: "#0A2FC4",
+      magenta: "#C839C5",
+      cyan: "#20C5C6",
+      white: "#C7C7C7",
+      lightBlack: "#686868",
+      lightRed: "#FD6F6B",
+      lightGreen: "#67F86F",
+      lightYellow: "#FFFA72",
+      lightBlue: "#6A76FB",
+      lightMagenta: "#FD7CFC",
+      lightCyan: "#68FDFE",
+      lightWhite: "#FFFFFF"
     },
 
     // The default width/height in pixels of a new window
@@ -104,30 +105,30 @@ module.exports = {
     // - Example: `C:\\Windows\\System32\\bash.exe`
     //
     // Git Bash on Windows
-    // `C:\\Program Files\\Git\\git-cmd.exe`
+    // - Example: `C:\\Program Files\\Git\\git-cmd.exe`
     //
     // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: 'C:\\Program Files\\Git\\git-cmd.exe',
+    shell: "C:\\Program Files\\Git\\git-cmd.exe",
 
     // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
     // by default ['--login'] will be used
-    shellArgs: ['--command=usr/bin/bash.exe', '-l', '-i'],
-    
+    shellArgs: ["--command=usr/bin/bash.exe", "-l", "-i"],
+
     // for environment variables
     //env: {},
-    env: {TERM: 'cygwin'},
-    env: {LANG: 'ja_JP.UTF-8'},
+    // env: {TERM: 'cygwin'},
+    env: { LANG: "ja_JP.UTF-8" },
 
     // set to `false` for no bell
     // bell: 'SOUND',
-    bell: 'false',
+    bell: "false",
 
     // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
     copyOnSelect: false,
 
     // if `true` (without backticks and without quotes), hyper will be set as the default protocol client for SSH
-    defaultSSHApp: true,
+    defaultSSHApp: true
 
     // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
     // selection is present (`true` by default on Windows and disables the context menu feature)
@@ -137,27 +138,27 @@ module.exports = {
     // bellSoundURL: 'http://example.com/bell.mp3',
 
     // for advanced config flags please refer to https://hyper.is/#cfg
-    overlay: {
-      alwaysOnTop: true,
-      animate: false,
-      hasShadow: false,
-      hideDock: true,
-      hideOnBlur: false,
-      hotkeys: ['Ctrl+F12'],
-      position: 'right',
-      primaryDisplay: false,
-      resizable: true,
-      startAlone: true,
-      startup: true,
-      size: 0.5,
-      tray: true,
-      unique: false,
-    },
+    // overlay: {
+    //   alwaysOnTop: true,
+    //   animate: false,
+    //   hasShadow: false,
+    //   hideDock: true,
+    //   hideOnBlur: false,
+    //   hotkeys: ['Ctrl+F12'],
+    //   position: 'right',
+    //   primaryDisplay: false,
+    //   resizable: true,
+    //   startAlone: true,
+    //   startup: true,
+    //   size: 0.5,
+    //   tray: true,
+    //   unique: false,
+    // },
 
-    opacity: {
-      focus: 0.85,
-      blur: 0.65,
-    }
+    // opacity: {
+    //   focus: 0.85,
+    //   blur: 0.65,
+    // }
   },
 
   // a list of plugins to fetch and install from npm
@@ -167,15 +168,15 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-        'hypercwd',
-        // 'hyperterm-overlay',
-        'hyperlinks',
-        'hyper-tab-icons',
-        'hyper-ayu',
-        'hyper-opacity',
-        // 'hyper-statusline',
-        'hyper-search',
-    ],
+    "hypercwd",
+    // 'hyperterm-overlay',
+    "hyperlinks",
+    "hyper-tab-icons",
+    "hyper-ayu",
+    // 'hyper-opacity', not working on Windows
+    // 'hyper-statusline',
+    "hyper-search"
+  ],
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
@@ -184,5 +185,5 @@ module.exports = {
   keymaps: {
     // Example
     // 'window:devtools': 'cmd+alt+o',
-  },
+  }
 };
